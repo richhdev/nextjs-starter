101445979cc8b9e30bfa1e7c613b9577af1ece0f
Richard Harris <79666708+richhdev@users.noreply.github.com>
2023-10-02 23:39:27 +1100

    ⬆️ update dependencies to latest (#1)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type {} from "styled-components/cssprop";
-import "@fontsource/oswald/variable.css";
-import "@fontsource/inconsolata/variable.css";
-import "@fontsource/nunito/variable.css";
+import "@fontsource-variable/oswald";
+import "@fontsource-variable/inconsolata";
+import "@fontsource-variable/nunito";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
